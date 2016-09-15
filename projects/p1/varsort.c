19f32dc7819c9812d489a5156f49cd3575ee1bfb
Haiyun Jin <haiyun@rockhopper-07.cs.wisc.edu>
2016-09-15 13:38:40 -0500

    Test with tests

--- projects/p1/varsort.c
+++ projects/p1/varsort.c
@@ -17,39 +17,62 @@
 #include <sys/stat.h>
 
 
-void usage(char *prog){
-  fprintf(stderr, "usage: %s <-i inFile> <-o outFile>\n", prog);
+void usage(char *prog) {
+// fprintf(stderr, "usage: %s <-i inFile> <-o outFile>\n", prog);
+  fprintf(stderr, "usage: varsort <-i inFile> <-o outFile>\n");
   exit(1);
 }
 
-int CompareDataptr(rec_dataptr_t *r1,rec_dataptr_t *r2) {
-    return (int) ( (*r1).key - (*r2).key ) ;
+// int CompareDataptr(const rec_dataptr_t *r1, const rec_dataptr_t *r2) {
+int CompareDataptr(const void *r1, const void *r2) {
+    const rec_dataptr_t *r1l = (const rec_dataptr_t *) r1;
+    const rec_dataptr_t *r2l = (const rec_dataptr_t *) r2;
+    return ((int) r1l->key) - ((int) r2l->key);
 }
 
+
 int main(int argc, char *argv[] ) {
-//    printf("Start\n");
     // arguments
-    char *inFile  =  "/no/such/file";
-    char *outFile =  "/no/such/file";
+    char *inFile, *outFile;
     int c;
-//    printf("after args\n");
 
-//    opterr = 0 ;
 
-     while ((c = getopt(argc, argv, "i:o:")) != -1) {
-       switch (c) {
-       case 'i':
-         inFile   = strdup(optarg);
-         break;
-       case 'o':
-          outFile = strdup(optarg);
-          break;
-       default:
+// /    if ( argc == 1 ) {
+// /        fprintf(stderr, "No argument\n");
+// /        usage(argv[0]);
+// /    }
+
+  // printf("# of argv: %d\n", argc);
+  // c = getopt(argc, argv, "io:");
+  // printf("c value: %d\n", c);
+
+
+     if ( argc == 5 ) {
+    while ((c = getopt(argc, argv, "i:o:")) != -1) {
+        printf(" I came here. \n");
+      switch (c) {
+      case 'i':
+        inFile   = strdup(optarg);
+        printf("Infile: %s\n", inFile);
+        break;
+      case 'o':
+        outFile = strdup(optarg);
+        printf("outfile: %s\n", outFile);
+        break;
+      default:
+        printf("Here comes the default\n");
+        usage(argv[0]);
+      }
+    }
+     } else if ( argc == 1 ) {
+         fprintf(stderr, "No argument\n");
          usage(argv[0]);
-       }
+     } else {
+         usage(argv[0]);
      }
 
-//     printf("Received args\n") ;
+
+
 
     // open input file
     int fin = open(inFile, O_RDONLY);
@@ -75,7 +98,6 @@ int main(int argc, char *argv[] ) {
         exit(1);
     }
 
-//    printf("Number of records: %d\n", recordsLeft);
     // output the number of keys as a header for this file
     rout = write(fout, &recordsLeft, sizeof(recordsLeft));
     if (rout != sizeof(recordsLeft)) {
@@ -84,56 +106,49 @@ int main(int argc, char *argv[] ) {
     // should probably remove file here but ...
     }
 
-//printf("After reading first line\n");
+// printf("After reading first line\n");
 
     int numRecords = recordsLeft;
-      rec_dataptr_t fullRecords[recordsLeft];
-//printf("After define fullrecords\n");
+// /      rec_dataptr_t fullRecords[recordsLeft];
+      rec_dataptr_t *fullRecords = malloc( sizeof(rec_dataptr_t)*recordsLeft);
+
+// printf("After define fullrecords\n");
       rec_nodata_t tempRecord;
-//printf("After define temprecords\n");
-//      unsigned int data[recordsLeft][MAX_DATA_INTS];
-//      unsigned int data[recordsLeft][15];
-//      unsigned int *data
-//      data = (unsigned int *) malloc(recordsLeft*MAX_DATA_INTS*sizeof(unsigned int)) ;
+// printf("After define temprecords\n");
 
-      unsigned int **data = malloc( sizeof(*data) * recordsLeft) ;
-      int i;
-//      if ( data ) {
-//        for ( i = 0 ; i < recordsLeft; ++i) {
-//            data[i] = malloc(sizeof(*data[i])*MAX_DATA_INTS) ;
-//        }
-//      }
+      unsigned int **data = malloc( sizeof(*data) * recordsLeft);
+// /      int i;
 
-//printf("Before entering read loop\n");
+// printf("Before entering read loop\n");
 
-      int count = 0 ;
+      int count = 0;
       // read all data into array?
       while (recordsLeft) {
         // read fix part
-          int data_size = sizeof(rec_nodata_t) ;
-          rin = read(fin, &tempRecord,data_size );
+          int data_size = sizeof(rec_nodata_t);
+          rin = read(fin, &tempRecord, data_size);
           if (rin != data_size) {
               perror("read");
               exit(1);
           }
-          //          assert(fixedHead.data_ints <= MAX_DATA_INTS);
           fullRecords[count].key = tempRecord.key;
           fullRecords[count].data_ints  = tempRecord.data_ints;
-          data[count] = malloc(sizeof(*data[count])*fullRecords[count].data_ints) ;
+          data[count] =
+              malloc(sizeof(*data[count])*fullRecords[count].data_ints);
 //////////////////////////////////////////////
 // For the above, can try
-//        rin = read(fin, &fullRecords[count], sizeof(rec_nodata_t));
-//        if (rin != sizeof(rec_nodata_t)) {
-//            perror("read");
-//            exit(1);
-//        }
+// 1       rin = read(fin, &fullRecords[count], sizeof(rec_nodata_t));
+// 1       if (rin != sizeof(rec_nodata_t)) {
+// 1           perror("read");
+// 1           exit(1);
+// 1       }
 // End try
 //////////////////////////////////////////////
 
           // read data
           data_size = fullRecords[count].data_ints * sizeof(unsigned int);
-          //rin = read(fin, &data[count][0],data_size);
-          rin = read(fin, data[count],data_size);
+          // rin = read(fin, &data[count][0],data_size);
+          rin = read(fin, data[count], data_size);
           if (rin != data_size) {
               perror("read");
               exit(1);
@@ -144,38 +159,40 @@ int main(int argc, char *argv[] ) {
           ++count;
       }
 
-//printf("After entering read loop\n");
-//recordsLeft = numRecords;
-//count=0;
-//while (recordsLeft) {
-//      printf("key %d: %u data_ints: %u rec: ", count, fullRecords[count].key, fullRecords[count].data_ints);
-//      int j;
-//      for (j = 0; j < fullRecords[count].data_ints; j++)
-//        printf("%u ", data[count][j]);
-//      printf("\n");
-//        --recordsLeft;
-//        ++count;
-//}
-//printf("\n");
+// printf("After entering read loop\n");
+// recordsLeft = numRecords;
+// count=0;
+// while (recordsLeft) {
+// /     printf("key %d: %u data_ints: %u rec: ",
+// count, fullRecords[count].key, fullRecords[count].data_ints);
+// /    int j;
+// /    for (j = 0; j < fullRecords[count].data_ints; j++)
+// /      printf("%u ", data[count][j]);
+// /    printf("\n");
+// /      --recordsLeft;
+// /      ++count;
+// }
+// printf("\n");
 
       // sort the key
-      qsort(fullRecords,numRecords,sizeof(rec_dataptr_t),CompareDataptr);
+      qsort(fullRecords, numRecords, sizeof(rec_dataptr_t), CompareDataptr);
 
       // print current
-//    recordsLeft = numRecords;
-//    count=0;
-//    while (recordsLeft) {
-//          printf("key %d: %u data_ints: %u rec: ", count, fullRecords[count].key, fullRecords[count].data_ints);
-//          int j;
-//          for (j = 0; j < fullRecords[count].data_ints; j++)
-//            printf("%u ", data[count][j]);
-//          printf("\n");
-//            --recordsLeft;
-//            ++count;
-//    }
+// /  recordsLeft = numRecords;
+// /  count=0;
+// /  while (recordsLeft) {
+// /        printf("key %d: %u data_ints: %u rec: ",
+// count, fullRecords[count].key, fullRecords[count].data_ints);
+// /        int j;
+// /        for (j = 0; j < fullRecords[count].data_ints; j++)
+// /          printf("%u ", data[count][j]);
+// /        printf("\n");
+// /          --recordsLeft;
+// /          ++count;
+// /  }
 
 
-//printf("After sort array\n");
+// printf("After sort array\n");
 
       // output data according to key
       recordsLeft = numRecords;
@@ -183,27 +200,36 @@ int main(int argc, char *argv[] ) {
       while (recordsLeft) {
           // output fixed part
           int data_size = sizeof(rec_nodata_t);
-          rout = write(fout,&fullRecords[count],data_size);
-            if (rout != data_size ) {
+          rout = write(fout, &fullRecords[count], data_size);
+            if ( rout != data_size ) {
               perror("write");
               exit(1);
             }
            // output data
            data_size = fullRecords[count].data_ints * sizeof(unsigned int);
-          rout = write(fout,(fullRecords[count].data_ptr),data_size);
-            if (rout != data_size ) {
+          rout = write(fout, (fullRecords[count].data_ptr), data_size);
+            if ( rout != data_size ) {
               perror("write");
               exit(1);
             }
               --recordsLeft;
               ++count;
       }
-//printf("After write data\n");
+// printf("After write data\n");
 
-
+      // free memory
+      recordsLeft = numRecords;
+      count = 0;
+      while (recordsLeft) {
+          free(data[count]);
+          --recordsLeft;
+          ++count;
+      }
       free(data);
       (void) close(fin);
       (void) close(fout);
+
+      return 0;
 }
 
 
