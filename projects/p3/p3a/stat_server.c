24d8942be29b9339254dca8bda9012fa1109e0f6
Haiyun Jin <haiyun@rockhopper-07.cs.wisc.edu>
2016-10-21 16:06:09 -0500

    Starting p3a

---
+++ projects/p3/p3a/stat_server.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+void usage() {
+    printf("Usage: stat_server -k key\n");
+    exit(1);
+}
+
+int main(int argc, char *argv[] ) {
+    if ( argc != 3 ) 
+        usage();
+    key_t key = (key_t) atoi(argv[2]);
+    if ( key == 0 ) {
+        perror("Invalid key\n");
+        exit(1);
+    }
+    size_t pagesize = (size_t) getpagesize();
+    int id  = shmget(key, pagesize, IPC_CREAT | SHM_R |SHM_W);
+    char* addr = (char*)shmat(id, NULL, 0);
+
+    while ( 1) {
+        printf("Message: ");
+        printf("%s\n", addr);
+        sleep(1);
+    }
+
+    return 0;   
+}
+
---
+++ projects/p3/p3a/stats_client.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+void usage() {
+    printf("Usage: stats_client -k key message\n");
+    exit(1);
+}
+
+int main(int argc, char *argv[] ) {
+
+    if ( argc != 4 ) {
+        usage();
+    }
+    key_t key = (key_t) atoi(argv[2]);
+    if ( key == 0 ) {
+        perror("Invalid key");
+        exit(1);
+    }
+
+    char *message = argv[3];
+
+//     key_t key = 100;
+    size_t pagesize = (size_t) getpagesize();
+
+    int id  = shmget(key, pagesize, IPC_CREAT | SHM_R |SHM_W);
+    char* addr = (char*)shmat(id, NULL, 0);
+
+    int i = 0;
+    while (message[i] != '\0' ) {
+        addr[i] = message[i];
+        ++i;
+    }
+    addr[i] = '\0';
+
+    return 0;
+}
+
